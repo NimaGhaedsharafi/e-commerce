597a2af7371bea0c49c9e561dfb72a2ae393073e
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2017-11-07 21:58:28 +0330

    to have more flexability a super class for TestCase has been declared

---
+++ tests/AppBundle/Controller/ApiTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nghaedsharafi
+ * Date: 11/7/17
+ * Time: 21:28
+ */
+
+namespace Tests\AppBundle\Controller;
+
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+abstract class ApiTest extends WebTestCase {}
