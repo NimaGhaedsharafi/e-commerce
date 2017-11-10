84a63141996ab698b31f94d1188d281f86705c27
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2017-11-10 14:33:24 +0330

    ack and abort response has been added to BaseController

--- src/AppBundle/Controller/BaseController.php
+++ src/AppBundle/Controller/BaseController.php
@@ -24,25 +24,51 @@ class BaseController extends Controller
     /**
      * Renders a view.
      *
-     * @param Renderable|string $data
+     * @param Renderable|string|null $data
      * @param int $status
      * @param array $headers
      *
      * @return Response A Response instance
      */
-    protected function response($data, $status = 200, $headers = [])
+    protected function response($data = null, $status = 200, $headers = [])
     {
-        if (is_array($data) && $data[0] instanceof Renderable) {
-
+        if (is_array($data) && isset($data[0]) && $data[0] instanceof Renderable) {
             $data = json_encode(array_map(function ($value) {
                 return $value instanceof Arrayable ? $value->toArray() : $value;
             }, $data));
         } elseif ($data instanceof Renderable) {
             $data = $data->render();
+        } elseif (is_array($data)) {
+            $data = json_encode($data);
+        } elseif ($data === null) {
+            if (floor($status / 200) == 1) {
+                $data = json_encode(['status' => 'ok']);
+            } else {
+                $data = json_encode(['status' => 'failed']);
+            }
         }
 
         $headers = array_merge(['content-type' => 'application/json'], $headers);
 
         return new Response($data, $status, $headers);
+    }
+
+    /**
+     * @param null $data
+     * @param int $status
+     * @param array $headers
+     * @return Response
+     */
+    protected function abort($data = null, $status = 400, $headers = [])
+    {
+        return $this->response($data, $status, $headers);
+    }
+
+    /**
+     * @return Response
+     */
+    protected function ack()
+    {
+        return $this->response(null, Response::HTTP_NO_CONTENT);
     }
 }
