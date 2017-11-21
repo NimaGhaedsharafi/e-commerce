f9e416d48c45e0ea727cf3a05bd2674e6936c350
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2017-11-21 20:06:39 +0330

    BaseEnum has been defined for further encapsulation

---
+++ src/AppBundle/Enums/BaseEnum.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nghaedsharafi
+ * Date: 11/21/17
+ * Time: 20:04
+ */
+
+namespace AppBundle\Enums;
+
+
+/**
+ * Class BaseEnum
+ * @package AppBundle\Enums
+ */
+class BaseEnum
+{
+
+}
