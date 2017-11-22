c7916449620aacbcf6a81800b77517a802ad2ae7
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2017-11-22 13:45:37 +0330

    ElasticSearch concrete has been defined

---
+++ src/AppBundle/Services/Search/ElasticSearch.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nghaedsharafi
+ * Date: 11/22/17
+ * Time: 13:40
+ */
+
+namespace AppBundle\Services\Search;
+
+
+/**
+ * Class ElasticSearch
+ * @package AppBundle\Services\Search
+ */
+class ElasticSearch implements SearchService
+{
+
+    /**
+     * @param $keyword
+     * @return array
+     */
+    public function search($keyword): array
+    {
+        // TODO: Implement search() method.
+    }
+
+    /**
+     * @param $id
+     * @param $data
+     * @return bool
+     */
+    public function add($id, $data): bool
+    {
+        // TODO: Implement add() method.
+    }
+}
